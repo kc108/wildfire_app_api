a4a09201129e2fe11bfd4957e1a2341b63ae1576
Kim Carpico <kcarpico@gmail.com>
2021-06-20 11:15:35 -0700

    completed delete and update routes

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -3,4 +3,23 @@ class SearchesController < ApplicationController
         searches = current_user.searches 
         render json: searches
     end
+
+    def update
+        search = current_user.searches.find(params[:id])
+        search.update(search_params)
+        render json: SearchSerializer.new(search)
+    end
+
+    def destroy
+        search = current_user.searches.find(params[:id])
+        search.destroy
+        render json: {}, status: :no_content
+    end
+
+    private
+
+    def search_params
+        params.require(:search).permit(:city)
+    end
+
 end
