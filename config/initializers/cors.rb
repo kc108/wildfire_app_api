54135ed430262863f62b562dd8cdcf16f7355d01
Kim Carpico <kcarpico@gmail.com>
2021-06-24 16:11:16 -0700

    fixing deployment issues

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -2,7 +2,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
-      origins '*'
+      origins 'https://windevent.netlify.app'
       resource '*', headers: :any, methods: [:get, :post, :patch, :put, :delete, :options]
     end
   end
