1efb3e22cbe003433740af09cce3338e1bbc5fa9
Kim Carpico <kcarpico@gmail.com>
2021-06-25 15:41:38 -0700

    changed to * config to test features

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -2,7 +2,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
-      origins 'https://windevent.netlify.app'
+      origins '*'
       resource '*', headers: :any, methods: [:get, :post, :patch, :put, :delete, :options]
     end
   end
